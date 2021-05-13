1d15d85212c620d4ebf26c0b8682e163c07b60e4
Langford <bankszhu4869@gmail.com>
2021-05-13 11:05:53 +0800

    ADD setting

--- pyccass/handlers/crawler_settings.py
+++ pyccass/handlers/crawler_settings.py
@@ -1,1 +1,17 @@
 LIST_URL = "https://www.hkexnews.hk/sdw/search/stocklist_c.aspx?sortby=stockcode&shareholdingdate="
+
+DATA_URL = 'https://www.hkexnews.hk/sdw/search/searchsdw_c.aspx'
+
+HEADER = {
+    'dnt': '1',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.85 Safari/537.36 Edg/90.0.818.46',
+    'origin': 'https://www.hkexnews.hk',
+    'referer': 'https://www.hkexnews.hk/sdw/search/searchsdw_c.aspx',
+    'sec-ch-ua': '" Not A;Brand";v="99", "Chromium";v="90", "Microsoft Edge";v="90"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-fetch-dest': 'document',
+    'sec-fetch-mode': 'navigate',
+    'sec-fetch-site': 'same-origin',
+    'sec-fetch-user': '?1',
+    'upgrade-insecure-requests': '1',
+}
