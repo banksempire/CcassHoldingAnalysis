7416e40502c57832f50d04cafc893f91e80d23a0
Langford <bankszhu4869@gmail.com>
2021-05-13 11:28:45 +0800

    ADD initialize_crawler

--- pyccass/handlers/crawler.py
+++ pyccass/handlers/crawler.py
@@ -1,21 +1,88 @@
 from datetime import datetime
+from typing import List, Tuple
 
 from requests import Session, get
 from bs4 import BeautifulSoup as bs
 
-import .crawler_settings as const
+import pyccass.handlers.crawler_settings as consts
 
 
-def get_stock_list(date: datetime) -> list:
-    stock_list_url = f"{const.LIST_URL}{date.strftime('%Y%m%d')}"
+def get_stock_list(date: datetime) -> List[Tuple]:
+    stock_list_url = f"{consts.LIST_URL}{date.strftime('%Y%m%d')}"
 
     response = get(stock_list_url)
 
     bsc = bs(response.content)
     stock_list = [
-        {
-            'code': tr.td.text.rstrip().lstrip(),
-            'name': tr.a.text.rstrip().lstrip()
-        } for tr in bsc.tbody.find_all('tr')
+        (
+            tr.td.text.rstrip().lstrip(),  # code
+            tr.a.text.rstrip().lstrip()  # name
+        ) for tr in bsc.tbody.find_all('tr')
     ]
     return stock_list
+
+
+def initialize_crawler():
+    '''
+    initialize crawler
+    '''
+
+    # Initialize session
+    session = Session()
+    session.headers.update(consts.HEADER)
+
+    url = consts.DATA_URL
+
+    # Get VIEWSTATE from website
+    response = session.post(url)
+    bsc = bs(response.content, features="html.parser")
+
+    __VIEWSTATEGENERATOR = bsc.find(
+        'input', id='__VIEWSTATEGENERATOR').get('value')
+    __VIEWSTATE = bsc.find(
+        'input', id='__VIEWSTATE').get('value')
+
+    def _parse_row(tr, date: datetime, stockcode: str):
+        row = tr.find_all('td')
+        content = (
+            int(date.timestamp()),
+            stockcode,
+            # custodian_code
+            row[0].find('div', class_="mobile-list-body").text,
+            # custodian
+            row[1].find('div', class_="mobile-list-body").text,
+            # holding
+            int(row[3].find('div', class_="mobile-list-body").text.replace(',', '')),
+            # holding_pct
+            float(row[4].find(
+                'div', class_="mobile-list-body").text.replace('%', '')),
+        )
+        return content
+
+    def get_data(qdate: datetime, stock: dict) -> List[Tuple]:
+        # Generate payload
+        code, name = stock
+
+        payload = {
+            '__EVENTTARGET': 'btnSearch',
+            '__EVENTARGUMENT': '',
+            '__VIEWSTATE': __VIEWSTATE,
+            '__VIEWSTATEGENERATOR': __VIEWSTATEGENERATOR,
+            'today': f"{datetime.now().strftime('%Y%m%d')}",
+            'sortBy': 'shareholding',
+            'sortDirection': 'desc',
+            'txtShareholdingDate': f"{qdate.strftime('%Y/%m/%d')}",
+            'txtStockCode': code,
+            'txtStockName': name,
+        }
+
+        # Get response
+        response = session.post(url, data=payload)
+        bsc = bs(response.content, features="html.parser")
+        # Parse response
+        tbody = bsc.tbody
+        tr = tbody.find_all('tr')[0]
+        table = [_parse_row(tr, qdate, code) for tr in tbody.find_all('tr')]
+        return table
+
+    return get_data
