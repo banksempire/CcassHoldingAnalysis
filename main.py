e6edb1620c96431d93611f7252f2ff11e0682d56
Langford <bankszhu4869@gmail.com>
2021-05-14 17:03:01 +0800

    sync

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
-from datetime import datetime
+from datetime import datetime, timedelta
+from typing import Callable
 
-from pyccass.handlers.crawler import initialize_crawler
+import requests
+
+from pyccass.handlers.crawler import initialize_crawler, get_stock_list
+from pyccass.handlers.database import DBHandler
+
+from pyccass.utils.proxies import get_proxies
+from requests import Session, get
+
+from queue import Queue
+
+from pyccass.tasks.update import async_update
+
+
+def main():
+    # get_data = initialize_crawler()
+    # get_data(datetime(2020, 4, 27), ('01413', '廣聯工程控股'))
+
+    # # get('https://www.baidu.com', proxies=get_proxies())
+    async_update()
+
 
 if __name__ == '__main__':
-    get_data = initialize_crawler()
-    data = get_data(datetime(2021, 5, 2), ('90519', '貴州茅台'))
-    print(data)
+    main()
