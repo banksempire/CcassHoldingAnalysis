650f02fdeb5af42fce7146fba40bac2f7c23b5b4
Langford <bankszhu4869@gmail.com>
2021-05-18 13:02:10 +0800

    ADD update bat

---
+++ update.bat
@@ -0,0 +1,3 @@
+cd "C:\Users\langford\Code\CcassHoldingAnalysis"
+call conda activate quant-dev
+call python update.py
---
+++ update.py
@@ -0,0 +1,9 @@
+from pyccass.tasks.update import async_update
+
+
+def main():
+    async_update(0.08)
+
+
+if __name__ == '__main__':
+    main()
